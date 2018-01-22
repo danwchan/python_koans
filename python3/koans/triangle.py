2b2da86400e3a053787953adc1436565fc3c32a8
danwchan <danwchan@gmail.com>
2018-01-21 21:16:09 -0400

    119 koans complete

--- python3/koans/triangle.py
+++ python3/koans/triangle.py
@@ -15,10 +15,28 @@
 #   about_triangle_project.py
 # and
 #   about_triangle_project_2.py
-#
+
 def triangle(a, b, c):
     # DELETE 'PASS' AND WRITE THIS CODE
-    pass
+    # pass
+    triangle = [a, b, c]
+    try:
+        if min(triangle) <= 0:
+            raise TriangleError("one or more sides are equal to or less than zero")
+        elif sum(sorted(triangle)[:2]) <= max(triangle):
+            raise TriangleError("any of the two sides are not equal to the other")
+        else:
+            pass
+    except TriangleError as ex:
+            raise ex
+    else:
+        trimetric = len(set(triangle))
+        if  trimetric == 1:
+            return "equilateral"
+        elif trimetric == 2:
+            return "isosceles"
+        else:
+            return "scalene"
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError(Exception):
