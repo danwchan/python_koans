f83f31433d6eb8b4f60f28b2c477c2c8085029dc
danwchan <dchan@intouchsol.com>
2018-12-04 16:25:28 -0500

    onto the extra credit assignment

--- python3/koans/about_extra_credit.py
+++ python3/koans/about_extra_credit.py
@@ -14,16 +14,35 @@ from runner.koan import *
 from about_scoring_project import score
 from about_dice_project import DiceSet
 
-class Player:
+class GreedPlayer:
     def __init__(self):
-        self.name = 'Default name, please set name of player'
+        self.name = None
 
 class GreedGame:
     def __init__(self):
         self.players = list()
+
+    def start_game(self, players = 2):
+
+        prompt = "> "
+        confrim = "n"
+
+        for playerid in ["player_{}".format(player) for player in range(1, players)]:
+            playerid = GreedPlayer()
+            while confrim != "y":
+                print("Welcome {} to a game of Greed!. Please enter your name".format(playerid))
+                playerid.name = input(prompt)
+                print("Hello, {}. Did I get that right? Are you ready to get greedy?".format(playerid.name))
+                if confirm == "y":
+                    break
 
 class AboutExtraCredit(Koan):
     # Write tests here. If you need extra test classes add them to the
     # test suite in runner/path_to_enlightenment.py
     def test_extra_credit_task(self):
         pass
+
+    def test_players_can_be_created_and_edited(self):
+        player1 = GreedPlayer()
+
+# Play a game of greed
--- python3/test.py
+++ python3/test.py
@@ -1,72 +1,26 @@
-class Proxy:
-    def __init__(self, target_object):
-        # WRITE CODE HERE
-        self._msglog = dict()
-        #initialize '_obj' attribute last. Trust me on this!
-        self._obj = target_object
-        print('init called and run')
+from runner.koan import *
+from koans.about_scoring_project import score
+from koans.about_dice_project import DiceSet
 
-    # WRITE CODE HERE
-    def __setattr__(self, msg_call, value):
-        if msg_call in ['_msglog', '_obj']:
-            
-#            print('setattr: {} exceptions for init'.format(msg_call))
-            return super().__setattr__(msg_call, value)
-        
-        else:
-            
-            if msg_call in self._msglog:
-                self._msglog[msg_call] += 1
-            else:
-                self._msglog.update({msg_call : 1})
-                
-#            print('setattr: running on the instance {}'.format(self._obj.__str__()))
-            return setattr(self._obj, msg_call, value)
-
-    def __getattr__(self, msg_call):
-        if msg_call in ['_msglog', '_obj']:
-            
-#            print('getattr: {} exceptions for init'.format(msg_call))
-            return super().__getattr__(msg_call)
-        
-        else:
-            
-            if msg_call in self._msglog:
-                self._msglog[msg_call] += 1
-            else:
-                self._msglog.update({msg_call : 1})
-
-#            print('getattr: running on the instance {}'.format(self._obj.__str__()))
-            return getattr(self._obj, msg_call)
-
-    def messages(self):
-        return [key for key in self._msglog.keys()]
-
-    def was_called(self, msg_call):
-        return msg_call in self._msglog
-
-    def number_of_times_called(self, msg_call):
-        return [count for key, count in self._msglog.items() if key == msg_call]
-
-# Example class using in the proxy testing above.
-class Television:
+class GreedPlayer:
     def __init__(self):
-        self._channel = None
-        self._power = None
+        self.name = None
 
-    @property
-    def channel(self):
-        return self._channel
+class GreedGame:
+    def __init__(self):
+        self.players = list()
 
-    @channel.setter
-    def channel(self, value):
-        self._channel = value
+    def start_game(self, players = 2):
 
-    def power(self):
-        if self._power == 'on':
-            self._power = 'off'
-        else:
-            self._power = 'on'
+        prompt = "> "
+        confirm = "n"
 
-    def is_on(self):
-        return self._power == 'on'
+        for playerid in ["player_{}".format(player) for player in range(1, players + 1)]:
+            playerid = GreedPlayer()
+            while confirm != "y":
+                print("Welcome to a game of Greed!. Please enter your name")
+                playerid.name = input(prompt)
+                print("Hello, {}. Did I get that right? Are you ready to get greedy?".format(playerid.name))
+                confirm = input(prompt)
+                if confirm == "y":
+                    break
